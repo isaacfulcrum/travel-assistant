a694a87e5d5e89ae6c6591ad0ad2edb428d2c376
Hiromi00 <55908313+hiromi00@users.noreply.github.com>
2020-05-01 11:38:07 -0500

    Mejor usaremos diccionarios para los registros

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from travel_assistant import *
 from PySide2.QtWidgets import QGraphicsScene
 from PySide2.QtCore import Slot
 from PySide2.QtGui import QPen, QColor, QTransform
-from country import Country
+import json
 
 
 class mainWindow(QMainWindow):
     def __init__(self):
         super(mainWindow, self).__init__()
+        self.countries = []
+        self.users = []
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.showMaximized()
@@ -19,19 +22,77 @@ class mainWindow(QMainWindow):
         self.scene = QGraphicsScene()
         self.ui.graphicsView.setScene(self.scene)
 
+
+
     def changePage(self):
         self.ui.views.setCurrentIndex(1)
 
+    def addNewCountry(self):
+        #Metodo de prueba para ver los datos que lleva cada diccionario de cada pais
+        #Hay que cambiarlo para que tenga un evento al momento de querer cargar uno nuevo
+        country = {
+            "name": "Mexico",
+            "starsClothes": 3,
+            "starsFood": 5,
+            "starsTourism": 4,
+            "cost": 465,
+            "coordinates": (50,35)
+        }
+
+        return country
+    #addNewCountry
+
+    def saveCountries(self):
+        fileLocation = QFileDialog.getSaveFileName(self, "SaveCountries", ".", "JSON")
+
+        with open(fileLocation[0], 'w') as file:
+            json.dump(self.countries, file, indent=5)
+    #saveCountries
+
+    def laodCountries(self):
+        fileLocation = QFileDialog.getOpenFileName(self, "LoadCountries", ".", "JSON(*.json)")
+
+        with open(fileLocation[0], 'r') as file:
+            self.countries = json.load(file)
+    #loadCountries
+
+    def addNewUser(self):
+        #Metodo de prueba para ver los datos que lleva cada diccionario de cada usuario
+        #Hay que cambiarlo para que tenga un evento al momento de querer cargar uno nuevo
+        country = {
+            "name": "Juan",
+            "capital": 1500,
+            "countryOrigin": "Mexico",
+            "countryDestiny": "Argentina",
+            "tasteForFood": 5,
+            "tasteForClothes": 3,
+            "tasteForTourism": 6
+        }
+
+        return country
+    #addNewCountry
+
+    def saveUsers(self):
+        fileLocation = QFileDialog.getSaveFileName(self, "SaveUsers", ".", "JSON")
+
+        with open(fileLocation[0], 'w') as file:
+            json.dump(self.users, file, indent=5)
+    #saveCountries
+
+    def loadUsers(self):
+        fileLocation = QFileDialog.getOpenFileName(self, "LoadUsers", ".", "JSON(*.json)")
+
+        with open(fileLocation[0], 'r') as file:
+            self.users = json.load(file)
+    #loadCountries
     @Slot()
     def drawGraph(self):
-        test = Country("Webos", "2", "3", "5", 200, (2, 5))
+        self.loadUsers()  # Se cargará el json de los usuarios
+        self.loadCountries()  # Se cargara el json de paises
         pen = QPen()
         pen.setWidth(3)
         color = QColor(12,43,22)
         pen.setColor(color)
-        coordinates = test.getCoordinates()
-        self.scene.addEllipse(coordinates[0], coordinates[0], 10, 10, pen)
-
     #drawGraph
 
 if __name__ == "__main__":
